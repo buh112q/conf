94be84a1ed0bb85cb08a99c9bfd0b9f21630991e
buh112q <110540141+buh112q@users.noreply.github.com>
2026-04-08 11:51:57 +0700

    Clean up core features configuration in niri.nix
    Removed commented-out lines for core features in niri.nix.

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -22,13 +22,12 @@
         enable = true;
         restartIfChanged = true;
       };
-      # Core features
-      enableSystemMonitoring = true; # System monitoring widgets (dgop)
-      enableVPN = true; # VPN management widget
-      enableDynamicTheming = true; # Wallpaper-based theming (matugen)
-      enableAudioWavelength = true; # Audio visualizer (cava)
-      enableCalendarEvents = true; # Calendar integration (khal)
-      enableClipboardPaste = true; # Pasting from the clipboard history (wtype)
+      enableSystemMonitoring = true;
+      enableVPN = true;
+      enableDynamicTheming = true;
+      enableAudioWavelength = true;
+      enableCalendarEvents = true;
+      enableClipboardPaste = true;
     };
     services.udisks2.enable = true;
     services.gvfs.enable = true;
