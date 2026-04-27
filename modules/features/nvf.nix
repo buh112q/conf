c240a2cf960e99f5d3d48776203cccaee7b7384c
buh112q <lnwpoom.zaa123@gmail.com>
2026-04-27 22:37:28 +0700

    add nvf module

--- modules/features/nvf.nix
+++ modules/features/nvf.nix
@@ -3,48 +3,49 @@
   inputs,
   ...
 }: {
-	flake.nixosModules.nvf = {pkgs,...}: {
-		imports = [
-			inputs.nvf.nixosModules.default
-		];
-		programs.nvf = {
-    			enable = true;
-    			settings = {
-      				vim = {
-                                        extraPackages = with pkgs; [
-                                                nixd
-                                                alejandra
-                                        ];
-					theme = {
-						enable = true;
-						name = "gruvbox";
-						style = "dark";
-					};
-					statusline.lualine.enable = true;
-					telescope.enable = true;
-					autocomplete.nvim-cmp.enable = true;
-				        lsp.enable = true;
-					
-                                        languages = {
-                                                enableFormat = true;
-						enableTreesitter = true;
+  flake.nixosModules.nvf = {pkgs, ...}: {
+    imports = [
+      inputs.nvf.nixosModules.default
+    ];
+    programs.nvf = {
+      enable = true;
+      settings = {
+        vim = {
+          extraPackages = with pkgs; [
+            nixd
+            alejandra
+          ];
+          theme = {
+            enable = true;
+            name = "gruvbox";
+            style = "dark";
+          };
+          statusline.lualine.enable = true;
+          telescope.enable = true;
+          autocomplete.nvim-cmp.enable = true;
+          lsp = {
+            enable = true;
+            formatOnSave = true;
+          };
+          languages = {
+            enableFormat = true;
+            enableTreesitter = true;
 
-						nix = {
-                                                        enable = true;
-                                                        format = {
-                                                                enable = true;
-                                                                type =
-                                                                        ["alejandra"];
-                                                                        formatOnSave = true; 
-                                                        }; lsp = { enable =
-                                                                true;
-                                                                servers = ["nixd"];
-                                                        };
-                                                };
-					};
-				};
-			};
-  		};
-	};
+            nix = {
+              enable = true;
+              format = {
+                enable = true;
+                type = ["alejandra"];
+              };
+              lsp = {
+                enable =
+                  true;
+                servers = ["nixd"];
+              };
+            };
+          };
+        };
+      };
+    };
+  };
 }
-
